5a1bf1cb62622a467a19323007d631c77e3006fd
dorisleong <dleong@csu.fullerton.edu>
2016-10-18 21:28:18 -0700

    Format js for JSHINT

--- assignment3/client/javascripts/app.js
+++ assignment3/client/javascripts/app.js
@@ -1,63 +1,78 @@
 var main = function () {
-    "use strict";
-
-    var sendAJAX = function (url, data, idResult) {
-        $.ajax({
-            url: url,
-            dataType: 'json',
-            type: "POST",
-            data: data,
-            contentType: "application/json",
-            success: function (response) {
-                $(idResult).text(response.result);
-            }
-        });
+  'use strict';
+  
+  var convertToInt = function (array) {
+    var i = 0;
+    for (; i < array.length; i++) {
+      array[i] = parseInt(array[i], 10);
     }
-
-    $('#avg').on('click', function (event) {
-        var input = $('#avgNums').val().split(',');
-        convertToInt(input);
-        sendAJAX('/avg', JSON.stringify({array: input}), '#avgResult');
+    return array;
+  };
+  
+  var sendAJAX = function (url, data, idResult) {
+    $.ajax({
+      url: url,
+      dataType: 'json',
+      type: 'POST',
+      data: data,
+      contentType: 'application/json',
+      success: function (response) {
+        $(idResult).text(response.result);
+      }
     });
+  };
 
-    $('#max').on('click', function (event) {
-        var input = $('#maxNums').val().split(',');
-        convertToInt(input);
-        sendAJAX('/max', JSON.stringify({array: input}), '#maxResult');
-    });
+  $('#avg').on('click', function (event) {
+    var input = $('#avgNums').val().split(',');
+    convertToInt(input);
+    sendAJAX('/avg', JSON.stringify({
+      array: input
+    }), '#avgResult');
+  });
 
-    $('#even').on('click', function (event) {
-        var input = $('#evenNums').val().split(',');
-        convertToInt(input);
-        sendAJAX('/even', JSON.stringify({array: input}), '#evenResult');
-    });
+  $('#max').on('click', function (event) {
+    var input = $('#maxNums').val().split(',');
+    convertToInt(input);
+    sendAJAX('/max', JSON.stringify({
+      array: input
+    }), '#maxResult');
+  });
 
-    $('#allEven').on('click', function (event) {
-        var input = $('#allEvenNums').val().split(',');
-        convertToInt(input);
-        sendAJAX('/allEven', JSON.stringify({array: input}), '#allEvenResult');
-    });
+  $('#even').on('click', function (event) {
+    var input = $('#evenNums').val().split(',');
+    convertToInt(input);
+    sendAJAX('/even', JSON.stringify({
+      array: input
+    }), '#evenResult');
+  });
 
-    $('#arrayContains').on('click', function (event) {
-        var inputArray = $('#containsArray').val().split(',');
-        var inputQuery = $('#containsQuery').val();
-        sendAJAX('/arrayContains', JSON.stringify({array: inputArray, query: inputQuery}), '#arrayContainsResult');
-    });
+  $('#allEven').on('click', function (event) {
+    var input = $('#allEvenNums').val().split(',');
+    convertToInt(input);
+    sendAJAX('/allEven', JSON.stringify({
+      array: input
+    }), '#allEvenResult');
+  });
 
-    $('#arrayContainsNTimes').on('click', function (event) {
-        var inputArray = $('#containsNTimesArray').val().split(',');
-        var inputQuery = $('#containsNTimesQuery').val();
-        var inputN = parseInt($('#nTimes').val(), 10);
-        sendAJAX('/arrayContainsNTimes', JSON.stringify({array: inputArray, query: inputQuery, n: inputN}), '#arrayContainsNTimesResult');
-    });
+  $('#arrayContains').on('click', function (event) {
+    var inputArray = $('#containsArray').val().split(',');
+    var inputQuery = $('#containsQuery').val();
+    sendAJAX('/arrayContains', JSON.stringify({
+      array: inputArray,
+      query: inputQuery
+    }), '#arrayContainsResult');
+  });
 
-    var convertToInt = function (array) {
-        var i = 0;
-        for (; i<array.length; i++) {
-            array[i] = parseInt(array[i], 10);
-        }
-        return array;
-    };
+  $('#arrayContainsNTimes').on('click', function (event) {
+    var inputArray = $('#containsNTimesArray').val().split(',');
+    var inputQuery = $('#containsNTimesQuery').val();
+    var inputN = parseInt($('#nTimes').val(), 10);
+    sendAJAX('/arrayContainsNTimes', JSON.stringify({
+      array: inputArray,
+      query: inputQuery,
+      n: inputN
+    }), '#arrayContainsNTimesResult');
+  });
 };
 
 $(document).ready(main);
--- assignment3/server.js
+++ assignment3/server.js
@@ -1,45 +1,68 @@
-var express = require("express"),
-    http = require("http"),
-    bodyParser = require('body-parser'),
-    array = require('./array.js'),
-    app = express();
+var express = require('express'),
+  http = require('http'),
+  bodyParser = require('body-parser'),
+  array = require('./array.js'),
+  app = express();
 
-app.use(express.static(__dirname + "/client"));
+app.use(express.static(__dirname + '/client'));
 
 // tell Express to parse incoming
 // JSON objects
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 http.createServer(app).listen(3000);
-console.log("Server running on port 3000");
+console.log('Server running on port 3000');
 
-app.post('/avg', function(req, res) {
-    var result = array.avg(req.body.array);
-    res.json({result: result});
+app.post('/avg', function (req, res) {
+  'use strict';
+  var result = array.avg(req.body.array);
+  res.json({
+    result: result
+  });
 });
 
-app.post('/max', function(req, res) {
-    var result = array.max(req.body.array);
-    res.json({result: result});
+app.post('/max', function (req, res) {
+  'use strict';
+  var result = array.max(req.body.array);
+  res.json({
+    result: result
+  });
 });
 
-app.post('/even', function(req, res) {
-    var result = array.even(req.body.array);
-    res.json({result: result});
+app.post('/even', function (req, res) {
+  'use strict';
+  var result = array.even(req.body.array);
+  res.json({
+    result: result
+  });
 });
 
-app.post('/allEven', function(req, res) {
-    var result = array.allEven(req.body.array);
-    res.json({result: result});
+app.post('/allEven', function (req, res) {
+  'use strict';
+  var result = array.allEven(req.body.array);
+  res.json({
+    result: result
+  });
 });
 
-app.post('/arrayContains', function(req, res) {
-    var result = array.arrayContains(req.body.array, req.body.query);
-    res.json({result: result});
+app.post('/arrayContains', function (req, res) {
+  'use strict';
+  var result = array.arrayContains(req.body.array, req.body.query);
+  res.json({
+    result: result
+  });
 });
 
-app.post('/arrayContainsNTimes', function(req, res) {
-    var result = array.arrayContainsNTimes(req.body.array, req.body.query, req.body.n);
-    res.json({result: result});
+app.post('/arrayContainsNTimes', function (req, res) {
+  'use strict';
+  var result = array.arrayContainsNTimes(
+    req.body.array, 
+    req.body.query, 
+    req.body.n);
+  res.json({
+    result: result
+  });
 });
