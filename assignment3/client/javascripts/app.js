148a6b178c4d6f9ff8a6e24b14791e4c039cecb9
dorisleong <dleong@csu.fullerton.edu>
2016-10-18 18:14:50 -0700

    Add ajax in client javascript

--- assignment3/client/javascripts/app.js
+++ assignment3/client/javascripts/app.js
@@ -3,7 +3,33 @@ var main = function () {
     $('#avg').click(function () {
         var avgInput = $(document.avgForm);
 
-    };
+        $.ajax({
+            url: "/avg",
+            dataType: 'json',
+            contentType: "application/json",
+            type: "POST",
+            data: JSON.stringify(avgInput.serializeArray()),
+            success: function (response) {
+                $("#avgResult").text(response.result);
+            }
+        })
+    });
+    
+    $('#max').click(function () {
+        var avgInput = $(document.avgForm);
+
+        $.ajax({
+            url: "/max",
+            dataType: 'json',
+            contentType: "application/json",
+            type: "POST",
+            data: JSON.stringify(avgInput.serializeArray()),
+            success: function (response) {
+                $("#maxResult").text(response.result);
+            }
+        })
+    });
+
     
 };
 
